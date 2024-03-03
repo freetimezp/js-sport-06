521641787f5d4fc9bbdbf8b03ff39ce62e69432e
freetimezp <kraftukrnet@ukr.net>
2024-03-03 13:50:19 +0200

    03. js-sport-06. Hero Component. Slider.

---
+++ components/CustomButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CustomButton = ({ text, containerStyles }: { text: string; containerStyles: string; }) => {
+    return (
+        <button className={`${containerStyles} group relative cursor-pointer overflow-hidden 
+            bg-accent uppercase`}>
+            <span className='ease absolute top-1/2 h-0 w-64 origin-center -translate-x-20
+                rotate-45 bg-black transition-all duration-300 group-hover:h-64 
+                group-hover:-translate-y-32'></span>
+
+            <span className='ease relative text-white transition-all duration-300 
+                group-hover:tracking-widest'>
+                {text}
+            </span>
+        </button>
+    );
+}
+
+export default CustomButton;
